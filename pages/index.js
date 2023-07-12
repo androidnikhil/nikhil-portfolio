f14a76248012c38785a33f73cfaab8507eaffd47
androidnikhil <nikhilpandey00999@gmail.com>
2023-07-12 11:35:55 +0530

    minor change

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="HI! Nikhil Pandey" />
+        <Header title="Hi! Nikhil Pandey" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
